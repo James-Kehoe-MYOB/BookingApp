5c5c9177e25e1dee4b9f28e8327ee836a5a5d490
James-Kehoe-MYOB <james.kehoe@myob.com>
2020-02-25 10:58:14 +1100

    fixed duplicate booking problem

--- Booking.cs
+++ Booking.cs
@@ -2,10 +2,12 @@ using System;
 
 namespace BookingApp {
     public class Booking {
-        private DateTime startTime;
+        public DateTime startTime { get; private set; }
+        public DateTime endTime { get; }
 
         public Booking(DateTime startTime, TimeSpan duration) {
             this.startTime = startTime;
+            endTime = startTime.Add(duration);
         }
     }
 }
--- ServiceProvider.cs
+++ ServiceProvider.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BookingApp {
     public class ServiceProvider {
@@ -13,9 +14,38 @@ namespace BookingApp {
         }
 
         public bool Book(Booking booking) {
-            Bookings.Add(booking);
-            return true;
+            
+            if (Bookings.Contains(booking, new BookingComparer())) {
+                
+                return false;
+            }
+            else {
+                Bookings.Add(booking);
+                return true;
+            }
         }
 
+    }
+
+    class BookingComparer : IEqualityComparer<Booking> {
+        public bool Equals(Booking x, Booking y) {
+            if (x == null || y == null) {
+                return false;
+            }
+
+            if (x.startTime == y.startTime) {
+                return true;
+            } 
+            
+            if (x.startTime < y.startTime && y.startTime < x.endTime) {
+                return true;
+            }
+
+            return false;
+        }
+
+        public int GetHashCode(Booking obj) {
+            throw new System.NotImplementedException();
+        }
     }
 }
